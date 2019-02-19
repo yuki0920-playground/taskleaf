8b706be3f957fe47ff30be72e1e875197c47e439
Yuki <ywsep20@yahoo.co.jp>
2019-02-19 22:48:08 +0000

    add destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @task = Task.new
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -11,6 +12,7 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
@@ -18,7 +20,18 @@ class TasksController < ApplicationController
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
+  
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+  end
   
   private
 
