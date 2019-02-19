ee7d7724cfebbe124febb23b42a2d48f03168dd2
Yuki <ywsep20@yahoo.co.jp>
2019-02-19 14:55:21 +0000

    add new&create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # resoucesで代用
+  # get 'tasks/index'
+  # get 'tasks/show'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  
+  root to: 'tasks#index'
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
