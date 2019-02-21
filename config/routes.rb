b403f5ec91e6f781ebd2cf104073a89a80c3ccc0
Yuki <ywsep20@yahoo.co.jp>
2019-02-21 23:02:11 +0000

    create Admin::UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   # resoucesで代用
   # get 'tasks/index'
   # get 'tasks/show'
