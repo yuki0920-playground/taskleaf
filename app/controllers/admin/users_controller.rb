af696239b9297db9f41d7dac609e3ef8f57f50c1
Yuki <ywsep20@yahoo.co.jp>
2019-02-22 22:25:58 +0000

    add users#index/show/edit/update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,19 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
 
   def edit
   end
-  
+
   def create
     @user = User.new(user_params)
 
@@ -16,11 +24,17 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-
-  def show
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
   end
 
-  def index
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザ「#{@user.name}」を削除しました。"
   end
   
   private
